d322ac60338d5293df35a14140d1df526094d2c3
Konstantin <konsti21@freenet.de>
2021-11-03 14:27:46 +0100

    optimize calculation time with numba

--- classes.py
+++ classes.py
@@ -1,9 +1,10 @@
+import numpy
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from scipy.spatial.distance import pdist, squareform
-# from numba import njit
+from numba import njit
 
 import util
 from util import printProgressBar
@@ -29,25 +30,10 @@ class Simulation:
         self.swimmerColors = np.random.rand(self.simulationConstants["numSwimmers"])
 
         # initialize states array and add initial values for first frame
-        self.states = np.empty((self.numFrames, self.simulationConstants["numSwimmers"], 3), dtype=float)
+        self.states = np.empty((self.numFrames, self.simulationConstants["numSwimmers"], 3), dtype=numpy.float64)
         for i in range(self.simulationConstants["numSwimmers"]):
-            swimmerState = np.array([[self.swimmers[i][0], self.swimmers[i][1], self.swimmers[i][2]]], dtype=float)
+            swimmerState = np.array([[self.swimmers[i][0], self.swimmers[i][1], self.swimmers[i][2]]], dtype=numpy.float64)
             self.states[0][i] = swimmerState
-
-        # initialize animation
-        # self.figure = plt.figure()
-        # self.figure.subplots_adjust(left=0, right=1, bottom=0, top=1)
-        # limits = self.simulationConstants["environmentSideLength"] / 2 + 0.2
-        # self.axis = self.figure.add_subplot(111, aspect='equal', autoscale_on=False,
-        #                      xlim=(-limits, limits), ylim=(-limits, limits))
-        #
-        # self.rect = plt.Rectangle((-self.simulationConstants["environmentSideLength"] / 2, -self.simulationConstants["environmentSideLength"] / 2),
-        #                      self.simulationConstants["environmentSideLength"],
-        #                      self.simulationConstants["environmentSideLength"],
-        #                      ec='none', lw=2, fc='none')
-        # self.axis.add_patch(self.rect)
-        #
-        # self.swimmerPlot = self.axis.scatter([], [])
 
     def simulate(self):
         for t in range (1, self.numFrames):
@@ -68,7 +54,7 @@ class Simulation:
         except IndexError:
             previousState = self.simulationStep(t - 1)
 
-        newState = previousState.copy()
+
 
         # _pdist = pdist(previousState[:, :2])
         distances = squareform(pdist(previousState[:, :2])) # symmetric matric with all distances between the particles
@@ -77,7 +63,24 @@ class Simulation:
         index1 = index1[uniqueDistance]
         index2 = index2[uniqueDistance]
 
-        for i in range(self.simulationConstants["numSwimmers"]):
+        newState = previousState.copy()
+
+        numSwimmers = self.simulationConstants["numSwimmers"]
+        environmentSideLength = self.simulationConstants["environmentSideLength"]
+        interactionRadius = self.simulationConstants["interactionRadius"]
+        randomAngleAmplitude = self.simulationConstants["randomAngleAmplitude"]
+        oscillationAmplitude = self.simulationConstants["oscillationAmplitude"]
+        fps = self.simulationConstants["fps"]
+        initialVelocity = self.simulationConstants["initialVelocity"]
+        return self.calcNewState(t, previousState, newState, index1, index2, numSwimmers, environmentSideLength, interactionRadius, randomAngleAmplitude, oscillationAmplitude, fps, initialVelocity)
+
+
+
+    @staticmethod
+    @njit
+    def calcNewState(t, previousState, newState, index1, index2, numSwimmers, environmentSideLength, interactionRadius, randomAngleAmplitude, oscillationAmplitude, fps, initialVelocity):
+        tau = 1 / fps
+        for i in range(numSwimmers):
             swimmerState = newState[i]
 
             # interaction over boundary stuff
@@ -85,68 +88,72 @@ class Simulation:
             # -> shadow teleport that particle over the boundary on the other side
             # get distances to all particles
             # (make it smart, so if in right upper corner, teleport to lower left)
-            leftInteractionBoundaryHit = (swimmerState[0] < -self.simulationConstants["environmentSideLength"] / 2 +
-                               self.simulationConstants["interactionRadius"])
-            rightInteractionBoundaryHit = (swimmerState[0] > self.simulationConstants["environmentSideLength"] / 2 -
-                                self.simulationConstants["interactionRadius"])
-            upInteractionBoundaryHit = (swimmerState[1] > self.simulationConstants["environmentSideLength"] / 2 -
-                             self.simulationConstants["interactionRadius"])
-            downInteractionBoundaryHit = (swimmerState[1] < -self.simulationConstants["environmentSideLength"] / 2 +
-                               self.simulationConstants["interactionRadius"])
+            leftInteractionBoundaryHit = (swimmerState[0] < -environmentSideLength / 2 +
+                                          interactionRadius)
+            rightInteractionBoundaryHit = (swimmerState[0] > environmentSideLength / 2 -
+                                           interactionRadius)
+            upInteractionBoundaryHit = (swimmerState[1] > environmentSideLength / 2 -
+                                        interactionRadius)
+            downInteractionBoundaryHit = (swimmerState[1] < -environmentSideLength / 2 +
+                                          interactionRadius)
 
-            shadowSwimmerPositions = np.empty((0, 3), float)
+            shadowSwimmerPositions = np.empty((0), dtype=numpy.float64)
             if leftInteractionBoundaryHit:
-                newShadowState = np.copy(swimmerState)
-                newShadowState[0] += self.simulationConstants["environmentSideLength"]
-                shadowSwimmerPositions = np.vstack((shadowSwimmerPositions, newShadowState))
+                newShadowState = swimmerState.copy()
+                newShadowState[0] += environmentSideLength
+                shadowSwimmerPositions = np.append(shadowSwimmerPositions, newShadowState)
 
                 if upInteractionBoundaryHit:
-                    newShadowStateUp = np.copy(swimmerState)
-                    newShadowStateUp[0] += self.simulationConstants["environmentSideLength"]
-                    newShadowStateUp[1] -= self.simulationConstants["environmentSideLength"]
-                    shadowSwimmerPositions = np.vstack((shadowSwimmerPositions, newShadowStateUp))
+                    newShadowStateUp = swimmerState.copy()
+                    newShadowStateUp[0] += environmentSideLength
+                    newShadowStateUp[1] -= environmentSideLength
+                    shadowSwimmerPositions = np.append(shadowSwimmerPositions, newShadowStateUp)
 
                 if downInteractionBoundaryHit:
-                    newShadowStateDown = np.copy(swimmerState)
-                    newShadowStateDown[0] += self.simulationConstants["environmentSideLength"]
-                    newShadowStateDown[1] += self.simulationConstants["environmentSideLength"]
-                    shadowSwimmerPositions = np.vstack((shadowSwimmerPositions, newShadowStateDown))
+                    newShadowStateDown = swimmerState.copy()
+                    newShadowStateDown[0] += environmentSideLength
+                    newShadowStateDown[1] += environmentSideLength
+                    shadowSwimmerPositions = np.append(shadowSwimmerPositions, newShadowStateDown)
             if rightInteractionBoundaryHit:
-                newShadowState = np.copy(swimmerState)
-                newShadowState[0] -= self.simulationConstants["environmentSideLength"]
-                shadowSwimmerPositions = np.vstack((shadowSwimmerPositions, newShadowState))
+                newShadowState = swimmerState.copy()
+                newShadowState[0] -= environmentSideLength
+                shadowSwimmerPositions = np.append(shadowSwimmerPositions, newShadowState)
 
                 if upInteractionBoundaryHit:
-                    newShadowStateUp = np.copy(swimmerState)
-                    newShadowStateUp[0] -= self.simulationConstants["environmentSideLength"]
-                    newShadowStateUp[1] -= self.simulationConstants["environmentSideLength"]
-                    shadowSwimmerPositions = np.vstack((shadowSwimmerPositions, newShadowStateUp))
+                    newShadowStateUp = swimmerState.copy()
+                    newShadowStateUp[0] -= environmentSideLength
+                    newShadowStateUp[1] -= environmentSideLength
+                    shadowSwimmerPositions = np.append(shadowSwimmerPositions, newShadowStateUp)
 
                 if downInteractionBoundaryHit:
-                    newShadowStateDown = np.copy(swimmerState)
-                    newShadowStateDown[0] -= self.simulationConstants["environmentSideLength"]
-                    newShadowStateDown[1] += self.simulationConstants["environmentSideLength"]
-                    shadowSwimmerPositions = np.vstack((shadowSwimmerPositions, newShadowStateDown))
+                    newShadowStateDown = swimmerState.copy()
+                    newShadowStateDown[0] -= environmentSideLength
+                    newShadowStateDown[1] += environmentSideLength
+                    shadowSwimmerPositions = np.append(shadowSwimmerPositions, newShadowStateDown)
             if upInteractionBoundaryHit:
-                newShadowState = np.copy(swimmerState)
-                newShadowState[1] -= self.simulationConstants["environmentSideLength"]
-                shadowSwimmerPositions = np.vstack((shadowSwimmerPositions, newShadowState))
+                newShadowState = swimmerState.copy()
+                newShadowState[1] -= environmentSideLength
+                shadowSwimmerPositions = np.append(shadowSwimmerPositions, newShadowState)
             if downInteractionBoundaryHit:
-                newShadowState = np.copy(swimmerState)
-                newShadowState[1] += self.simulationConstants["environmentSideLength"]
-                shadowSwimmerPositions = np.vstack((shadowSwimmerPositions, newShadowState))
+                newShadowState = swimmerState.copy()
+                newShadowState[1] += environmentSideLength
+                shadowSwimmerPositions = np.append(shadowSwimmerPositions, newShadowState)
 
+            # shadowSwimmerPositions is up to this point a 1-D array with all the positions and velocity angles like [x1, y1, phi1, x2, y2, phi2, ..., xn, yn, phin]
+            # reshape it into a 2-D array: [[x1, y1, phi1], [x2, y2, phi2], ..., [xn, yn, phin]]
+            shadowSwimmerPositions = np.reshape(shadowSwimmerPositions, (-1, 3))
             numberOfShadowPositions = len(shadowSwimmerPositions)
             if numberOfShadowPositions > 0:
-                for j in range(self.simulationConstants["numSwimmers"]):
+                for j in range(numSwimmers):
                     if i == j:
                         continue
 
                     for k in range(numberOfShadowPositions):
                         shadowPosition = shadowSwimmerPositions[k]
                         shadowDistanceSwimmer = previousState[j]
-                        distanceBetween = (shadowPosition[0] - shadowDistanceSwimmer[0])**2 + (shadowPosition[1] - shadowDistanceSwimmer[1])**2
-                        if distanceBetween**2 <= self.simulationConstants["interactionRadius"]:
+                        distanceBetween = (shadowPosition[0] - shadowDistanceSwimmer[0]) ** 2 + (
+                                    shadowPosition[1] - shadowDistanceSwimmer[1]) ** 2
+                        if distanceBetween ** 2 <= interactionRadius:
                             index1 = np.append(index1, [i])
                             index2 = np.append(index2, [j])
 
@@ -161,50 +168,51 @@ class Simulation:
                 cosSum += np.cos(swimmerInRange[2])
 
             averageAngle = np.arctan2(sinSum, cosSum)
-            randomAngle = (np.random.rand() - 0.5) * self.simulationConstants["randomAngleAmplitude"]
-            sinusOscillation = np.pi / 40 * np.sin((t / (2 * self.simulationConstants["fps"])) * 2 * np.pi)
+            randomAngle = (np.random.rand() - 0.5) * randomAngleAmplitude
+            sinusOscillation = oscillationAmplitude * np.sin(
+                (t / (2 * fps)) * 2 * np.pi)
             swimmerState[2] = averageAngle + randomAngle + sinusOscillation
 
-            xVel = self.simulationConstants["initialVelocity"] * np.cos(swimmerState[2])
-            yVel = self.simulationConstants["initialVelocity"] * np.sin(swimmerState[2])
+            xVel = initialVelocity * np.cos(swimmerState[2])
+            yVel = initialVelocity * np.sin(swimmerState[2])
 
-            swimmerState[0] += xVel * self.tau
-            swimmerState[1] += yVel * self.tau
+            swimmerState[0] += xVel * tau
+            swimmerState[1] += yVel * tau
 
             # check for boundary hits
-            leftBoundaryHit = (swimmerState[0] <= -self.simulationConstants["environmentSideLength"] / 2)
-            rightBoundaryHit = (swimmerState[0] >= self.simulationConstants["environmentSideLength"] / 2)
-            upBoundaryHit = (swimmerState[1] >= self.simulationConstants["environmentSideLength"] / 2)
-            downBoundaryHit = (swimmerState[1] <= -self.simulationConstants["environmentSideLength"] / 2)
+            leftBoundaryHit = (swimmerState[0] <= -environmentSideLength / 2)
+            rightBoundaryHit = (swimmerState[0] >= environmentSideLength / 2)
+            upBoundaryHit = (swimmerState[1] >= environmentSideLength / 2)
+            downBoundaryHit = (swimmerState[1] <= -environmentSideLength / 2)
 
-            # vvec = np.array([np.cos(swimmerState[2]), np.sin(swimmerState[2])])
-            # n = np.array([0, 0])
             if leftBoundaryHit:
-                swimmerState[0] += (self.simulationConstants["environmentSideLength"])
-                # n = np.array([1, 0])
-                # swimmerState[0] += self.simulationConstants["swimmerSize"] / 4
+                swimmerState[0] += (environmentSideLength)
             if rightBoundaryHit:
-                swimmerState[0] += -(self.simulationConstants["environmentSideLength"])
-                # n = np.array([-1, 0])
-                # swimmerState[0] -= self.simulationConstants["swimmerSize"] / 4
+                swimmerState[0] += -(environmentSideLength)
             if upBoundaryHit:
-                swimmerState[1] += -(self.simulationConstants["environmentSideLength"])
-                # n = np.array([0, -1])
-                # swimmerState[1] -= self.simulationConstants["swimmerSize"] / 4
+                swimmerState[1] += -(environmentSideLength)
             if downBoundaryHit:
-                swimmerState[1] += (self.simulationConstants["environmentSideLength"])
-                # n = np.array([0, 1])
-                # swimmerState[1] += self.simulationConstants["swimmerSize"] / 4
-
-            # if leftBoundaryHit or rightBoundaryHit or upBoundaryHit or downBoundaryHit:
-                # http://www.3dkingdoms.com/weekly/weekly.php?a=2
-                # vvec = -2 * (np.dot(vvec, n)) * n + vvec
-                # swimmerState[2] = np.arctan2(vvec[1], vvec[0]) % (2 * np.pi)
-            
+                swimmerState[1] += (environmentSideLength)
 
         return newState
 
+
     def animate(self):
+        # initialize animation
+        self.figure = plt.figure()
+        self.figure.subplots_adjust(left=0, right=1, bottom=0, top=1)
+        limits = self.simulationConstants["environmentSideLength"] / 2 + 0.2
+        self.axis = self.figure.add_subplot(111, aspect='equal', autoscale_on=False,
+                             xlim=(-limits, limits), ylim=(-limits, limits))
+
+        self.rect = plt.Rectangle((-self.simulationConstants["environmentSideLength"] / 2, -self.simulationConstants["environmentSideLength"] / 2),
+                             self.simulationConstants["environmentSideLength"],
+                             self.simulationConstants["environmentSideLength"],
+                             ec='none', lw=2, fc='none')
+        self.axis.add_patch(self.rect)
+
+        self.swimmerPlot = self.axis.scatter([], [])
+
         def plotInit():
             # microswimmersPlot.set_offsets([], [])
             self.rect.set_edgecolor('none')
