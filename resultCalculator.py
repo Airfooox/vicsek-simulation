fc961bbb0bb3912a7ef053e1bf8d78b0447a9ae9
Airfox <konstantin.zisiadis@st.ovgu.de>
2021-11-12 19:07:36 +0100

    add progressbar to result calculation

--- resultCalculator.py
+++ resultCalculator.py
@@ -2,6 +2,7 @@ import multiprocessing as mp
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 def calculateVelocity(dir):
     with open(dir + '/constants.txt') as constantsFile:
@@ -27,7 +28,7 @@ def calculateResult(calculationData):
     dict = calculationData[2]
 
     dirOfData = dir + '/' + str(num)
-    subSimulationRange = range(1, 6)
+    subSimulationRange = range(50)
 
     with open(dirOfData + '_1' + '/constants.txt') as constantsFile:
         constants = json.load(constantsFile)
@@ -41,35 +42,36 @@ def calculateResult(calculationData):
 
 if __name__ == "__main__":
     manager = mp.Manager()
-    sameRhoDirectory = manager.dict()
-    # sameEtaRange = range(1, 501)
-    sameRhoRange = range(0, 101)
+    sameEtaDirectory = manager.dict()
+    sameEtaRange = range(150)
 
-    # calculateResult(['E:/simulationdata/sameRho', str(1), sameRhoDirectory])
-
-    sameRhoPool = []
-    for i in sameRhoRange:
-        sameRhoPool.append(['E:/simulationdata/sameRho', i, sameRhoDirectory])
+    sameEtaPool = []
+    for i in sameEtaRange:
+        sameEtaPool.append(['E:/simulationdata/sameEtaGroup', i, sameEtaDirectory])
 
     pool = mp.Pool(processes=(mp.cpu_count() - 2))
-    pool.map(calculateResult, sameRhoPool)
+    for _ in tqdm(pool.imap(func=calculateResult, iterable=sameEtaPool),
+                  total=len(sameEtaPool)):
+        pass
     pool.close()
     pool.join()
 
     def takeKeyToSort(element):
         return element[0]
 
-    sortedDict = sorted(sameRhoDirectory.items(), key = lambda x : x[0])
+    sortedDict = sorted(sameEtaDirectory.items(), key = lambda x : x[0])
     x, y = [], []
 
     for entry in sortedDict:
         index = entry[0]
         result = entry[1]
-        # print(index)
-        x.append(result['constants']['randomAngleAmplitude'])
+        # print(result['constants']['numSwimmers'], result['va'])
+        x.append(result['constants']['numSwimmers']/(result['constants']['environmentSideLength'])**2)
+        # x.append(result['constants']['randomAngleAmplitude'])
         y.append(result['va'])
 
     plt.plot(x, y, 'ro')
+    # plt.xlabel(r'$\rho \longrightarrow$')
     plt.xlabel(r'$\eta \longrightarrow$')
     plt.ylabel(r'$v_a \longrightarrow$')
     plt.grid('both')
