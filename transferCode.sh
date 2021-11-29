696fc2ce5f01921500d0dcd7dd4b88b10c303022
Konstantin <konsti21@freenet.de>
2021-11-29 15:34:17 +0100

    bash script to upload code

---
+++ transferCode.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+selectUplaodTarget() {
+    echo "Select target computer to uoload:"
+    echo "(ka)enguru, (ko)ala, (s)patz"
+    read TARGET 
+
+    echo "The target computer is: $TARGET"
+
+    case $TARGET in
+        "ka" | "kaenguru")
+            scp *.py kzisiadis@twmb-kaenguru.nat.uni-magdeburg.de:~/code/vicsek-simulation/
+            ;;
+
+        "ko" | "koala")
+            scp *.py kzisiadis@twmb-koala.nat.uni-magdeburg.de:~/code/vicsek-simulation/
+            ;;
+        
+        "s" | "spatz")
+            scp *.py kzisiadis@twmb-spatz.nat.uni-magdeburg.de:~/code/vicsek-simulation/
+            ;;
+
+        *)
+            echo "Specified target unknown"
+            selectUplaodTarget
+            ;;
+    esac
+}
+
+selectUplaodTarget
