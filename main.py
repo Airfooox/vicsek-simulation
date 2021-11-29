8984a399e418a4a6c1e228e5da4ffc6a225d2171
Konstantin <konsti21@freenet.de>
2021-11-29 15:35:09 +0100

    change simulation scenario code

--- main.py
+++ main.py
@@ -6,98 +6,115 @@ import time
 
 from simulationManager import SimulationManager, SimulationGroup
 
-
-def constantFunc(environmentSideLength=7, numSwimmers=1, randomAngleAmplitude=2, oscillationAmplitude=np.pi / 40):
-    return {
-        "fps": 30,
-        "time": 60,
-        "environmentSideLength": environmentSideLength,
-        "numSwimmers": numSwimmers,
-        "initialVelocity": 0.1,
-        "interactionRadius": 0.1,
-        "randomAngleAmplitude": randomAngleAmplitude,  # eta
-        "oscillationAmplitude": oscillationAmplitude,
-
-        "swimmerSize": 0.04,
-        "saveVideo": False
-    }
-
-
 if __name__ == "__main__":
-    starttime = time.time()
+    starttime = time.perf_counter()
 
-    simulationManager = SimulationManager('E:/simulationdata')
+    simulationDir = '/local/kzisiadis/vicsek-simulation'
+    # simulationDir = 'D:/simulationdata'
+    simulationManager = SimulationManager(simulationDir)
 
 
-    def sameEtaConstants(i, numSimulation, time, fps):
+    # def sameEtaConstants(i, numSimulation, time, fps):
+    #     return {
+    #         "time": time,
+    #         "fps": fps,
+    #
+    #         "environmentSideLength": 7,
+    #         "numSwimmers": i + 1,
+    #         "interactionRadius": 1,
+    #         "randomAngleAmplitude": 2,
+    #         "oscillationAmplitude": np.pi / 40,
+    #
+    #         "initialVelocity": 0.075,
+    #         "swimmerSize": 0.04,
+    #
+    #         "saveVideo": False,
+    #     }
+    #
+    #
+    # sameEtaGroup = SimulationGroup(simulationDataDir=simulationDir + '/sameEtaGroup', constantsFunc=sameEtaConstants, numSimulation=500, repeatNum=50, saveTrajectoryData=False)
+    # simulationManager.appendGroup(sameEtaGroup)
+
+    def sameRhoConstants400(i, numSimulation, time, fps):
         return {
             "time": time,
             "fps": fps,
 
-            "environmentSideLength": 7,
-            "numSwimmers": i + 1,
+            "environmentSideLength": 10,
+            "numSwimmers": 400,
             "interactionRadius": 1,
-            "randomAngleAmplitude": 2,
-            "oscillationAmplitude": np.pi / 40,
+            "randomAngleAmplitude": 5 * (i / numSimulation),
+            # "oscillationAmplitude": np.pi / 40,
+            "oscillationAmplitude": 0,
 
             "initialVelocity": 0.075,
             "swimmerSize": 0.04,
-            "saveVideo": False
+
+            "saveVideo": False,
         }
 
 
-    sameEtaGroup = SimulationGroup('E:/simulationdata/sameEtaGroup', sameEtaConstants, 150, 50)
+    sameRhoGroup400 = SimulationGroup(simulationDataDir=simulationDir + '/sameRhoGroup400',
+                                      constantsFunc=sameRhoConstants400, numSimulation=50, repeatNum=50,
+                                      saveTrajectoryData=False)
+    # simulationManager.appendGroup(sameRhoGroup400)
 
-    simulationManager.appendGroup(sameEtaGroup)
+    def sameRhoConstants1000(i, numSimulation, time, fps):
+        return {
+            "time": 7 * time,
+            "fps": fps,
+
+            "environmentSideLength": 15.8113,
+            "numSwimmers": 1000,
+            "interactionRadius": 1,
+            "randomAngleAmplitude": 5 * (i / numSimulation),
+            # "oscillationAmplitude": np.pi / 40,
+            "oscillationAmplitude": 0,
+
+            "initialVelocity": 0.075,
+            "swimmerSize": 0.04,
+
+            "saveVideo": False,
+        }
+
+
+    sameRhoGroup1000 = SimulationGroup(simulationDataDir=simulationDir + '/sameRhoGroup1000',
+                                      constantsFunc=sameRhoConstants1000, numSimulation=50, repeatNum=50,
+                                      saveTrajectoryData=False)
+    simulationManager.appendGroup(sameRhoGroup1000)
+
+
+    def sameRhoConstants4000(i, numSimulation, time, fps):
+        return {
+            "time": 10 * time,
+            "fps": fps,
+
+            "environmentSideLength": 31.6,
+            "numSwimmers": 4000,
+            "interactionRadius": 1,
+            "randomAngleAmplitude": 5 * (i / numSimulation),
+            # "oscillationAmplitude": np.pi / 40,
+            "oscillationAmplitude": 0,
+
+            "initialVelocity": 0.075,
+            "swimmerSize": 0.04,
+
+            "saveVideo": False,
+        }
+
+
+    sameRhoGroup4000 = SimulationGroup(simulationDataDir=simulationDir + '/sameRhoGroup4000',
+                                       constantsFunc=sameRhoConstants4000, numSimulation=50, repeatNum=50,
+                                       saveTrajectoryData=False)
+    # simulationManager.appendGroup(sameRhoGroup4000)
+
     simulationManager.simulate()
 
-    # constantsArraySameEtaConfigs = []
-    # for i in range(1, 101):
-    #     constantsConfigWithoutOscillation = constantFunc(4, i, 2, 0)
-    #     constantsConfigWithOscillation = constantFunc(4, i, 2)
-    #
-    #     constantsArraySameEtaConfigs.append(['E:/simulationdata/r = 0,1/sameEtaWOOscillation', str(i) + '_1', constantsConfigWithoutOscillation])
-    #     constantsArraySameEtaConfigs.append(['E:/simulationdata/r = 0,1/sameEtaWOOscillation', str(i) + '_2', constantsConfigWithoutOscillation])
-    #     constantsArraySameEtaConfigs.append(['E:/simulationdata/r = 0,1/sameEtaWOOscillation', str(i) + '_3', constantsConfigWithoutOscillation])
-    #     constantsArraySameEtaConfigs.append(['E:/simulationdata/r = 0,1/sameEtaWOOscillation', str(i) + '_4', constantsConfigWithoutOscillation])
-    #     constantsArraySameEtaConfigs.append(['E:/simulationdata/r = 0,1/sameEtaWOOscillation', str(i) + '_5', constantsConfigWithoutOscillation])
-    #
-    #     constantsArraySameEtaConfigs.append(['E:/simulationdata/r = 0,1/sameEtaWOscillation', str(i) + '_1', constantsConfigWithOscillation])
-    #     constantsArraySameEtaConfigs.append(['E:/simulationdata/r = 0,1/sameEtaWOscillation', str(i) + '_2', constantsConfigWithOscillation])
-    #     constantsArraySameEtaConfigs.append(['E:/simulationdata/r = 0,1/sameEtaWOscillation', str(i) + '_3', constantsConfigWithOscillation])
-    #     constantsArraySameEtaConfigs.append(['E:/simulationdata/r = 0,1/sameEtaWOscillation', str(i) + '_4', constantsConfigWithOscillation])
-    #     constantsArraySameEtaConfigs.append(['E:/simulationdata/r = 0,1/sameEtaWOscillation', str(i) + '_5', constantsConfigWithOscillation])
-    #
-    # constantsArraySameRhoProcesses = []
-    # for i in range(0, 101):
-    #     constantsConfigWithoutOscillation = constantFunc(5, 100, 5 * (i / 100), 0)
-    #     constantsConfigWithOscillation = constantFunc(5, 100, 5 * (i / 100))
-    #
-    #     constantsArraySameRhoProcesses.append(['E:/simulationdata/r = 0,1/sameRhoWOOscillation', str(i) + '_1', constantsConfigWithoutOscillation])
-    #     constantsArraySameRhoProcesses.append(['E:/simulationdata/r = 0,1/sameRhoWOOscillation', str(i) + '_2', constantsConfigWithoutOscillation])
-    #     constantsArraySameRhoProcesses.append(['E:/simulationdata/r = 0,1/sameRhoWOOscillation', str(i) + '_3', constantsConfigWithoutOscillation])
-    #     constantsArraySameRhoProcesses.append(['E:/simulationdata/r = 0,1/sameRhoWOOscillation', str(i) + '_4', constantsConfigWithoutOscillation])
-    #     constantsArraySameRhoProcesses.append(['E:/simulationdata/r = 0,1/sameRhoWOOscillation', str(i) + '_5', constantsConfigWithoutOscillation])
-    #
-    #     constantsArraySameRhoProcesses.append(['E:/simulationdata/r = 0,1/sameRhoWOscillation', str(i) + '_1', constantsConfigWithOscillation])
-    #     constantsArraySameRhoProcesses.append(['E:/simulationdata/r = 0,1/sameRhoWOscillation', str(i) + '_2', constantsConfigWithOscillation])
-    #     constantsArraySameRhoProcesses.append(['E:/simulationdata/r = 0,1/sameRhoWOscillation', str(i) + '_3', constantsConfigWithOscillation])
-    #     constantsArraySameRhoProcesses.append(['E:/simulationdata/r = 0,1/sameRhoWOscillation', str(i) + '_4', constantsConfigWithOscillation])
-    #     constantsArraySameRhoProcesses.append(['E:/simulationdata/r = 0,1/sameRhoWOscillation', str(i) + '_5', constantsConfigWithOscillation])
-    #
-    # poolConfigMerge = constantsArraySameEtaConfigs + constantsArraySameRhoProcesses
-    # pool = mp.Pool(processes=(mp.cpu_count() - 2))
-    # pool.map(runSimulation, poolConfigMerge)
-    # pool.close()
-    #
-    # print('That took {} seconds'.format(time.time() - starttime))
-
-    # runSimulation('./results/sameRho', 1, CONSTANTS )
-
-    # constants = constantFunc(31.5, 4000, 0.5)
+    # constants = sameEtaConstants(1, 1, 15, 1)
     # simulation = Simulation(constants)
-    # printProgressBar(0, simulation.numFrames, prefix='Simulation Progress:', suffix='Simulation Complete', length=50)
+    # # printProgressBar(0, simulation.numFrames, prefix='Simulation Progress:', suffix='Simulation Complete', length=50)
     # simulation.simulate()
+    # simulation.getAbsoluteVelocityTotal()
     # states = simulation.states
-    print('That took {} seconds'.format(time.time() - starttime))
+    print('That took {} seconds'.format(time.perf_counter() - starttime))
     # simulation.animate()
